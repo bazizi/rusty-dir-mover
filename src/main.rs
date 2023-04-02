528bf9261addede6bd23e3de47b5953d613182a6
Behnam Azizi <bazizi@sfu.ca>
2023-04-02 16:25:09 -0700

    working code

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
-use std::fs::rename;
+use fs_extra::dir::{move_dir, CopyOptions};
 use std::os::windows::fs::symlink_dir;
-use std::path::{Component, Components};
+use std::process::Command;
 
 fn main() {
+    println!("\n\n");
     let mut args = std::env::args();
     let _exe_path = args.next();
     let dir_to_move = args.next();
     let dest_dir = args.next();
     if dir_to_move.is_none() || dest_dir.is_none() {
         panic!(
-            r#"\n\nUsage: {} <path/to/dir/to/clean> <path/to/dest/dir>\n\n"#,
+            r#"Usage: {} <path/to/dir/to/clean> <path/to/dest/dir>\n\n"#,
             _exe_path.unwrap()
         );
     }
@@ -25,6 +26,10 @@ fn main() {
         .as_os_str()
         .to_str()
         .expect("Failed to convert os str to str");
+    println!(
+        "The name of the directory to move is [{}]...",
+        dir_to_move_name
+    );
     let dest_dir_including_source_dir_name = dest_dir.join(dir_to_move_name);
     let dest_dir_including_source_dir_name = dest_dir_including_source_dir_name.to_str().unwrap();
 
@@ -32,14 +37,16 @@ fn main() {
     // let path_dir_to_move = std::path::Path::new(&dir_to_move);
     println!(
         "Moving [{}] to [{}] ...",
-        dir_to_move_name, dest_dir_including_source_dir_name
+        dir_to_move.as_os_str().to_str().unwrap(),
+        dest_dir_including_source_dir_name
     );
 
-    rename(&dir_to_move, dest_dir_including_source_dir_name).expect(&format!(
-        "\n\nfailed to move directory [{}] to [{}]\n\n",
-        dir_to_move.to_str().unwrap(),
-        dest_dir_including_source_dir_name
-    ));
+    move_dir(
+        dir_to_move.as_os_str().to_str().unwrap(),
+        dest_dir,
+        &CopyOptions::new(),
+    )
+    .expect("Failed to move");
 
     // create a link to the dir moved to the dest dir
     println!(
